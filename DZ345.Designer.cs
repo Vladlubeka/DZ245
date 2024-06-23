90b3664d6da299c3bda33daab1e10c121f4c91b9
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2024-06-23 18:39:37 +0300

    Add files via upload

---
+++ DZ345.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DrawCircleApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label lblPoints;
+        private Label lblRadius;
+        private Label lblArea;
+        private Button btnReset;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(12, 35);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(60, 20);
+            this.lblPoints.TabIndex = 0;
+            this.lblPoints.Text = "Points:";
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(12, 65);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(61, 20);
+            this.lblRadius.TabIndex = 1;
+            this.lblRadius.Text = "Radius:";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(12, 95);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(45, 20);
+            this.lblArea.TabIndex = 2;
+            this.lblArea.Text = "Area:";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 9);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 30);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.lblPoints);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ DZ345.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawCircleApp
+{
+    public partial class Form1 : Form
+    {
+        private Point? centerPoint = null;
+        private Point? circumferencePoint = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.Text = "Draw Circle App";
+            this.MouseClick += new MouseEventHandler(Form1_MouseClick);
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (centerPoint == null)
+            {
+                centerPoint = e.Location;
+                lblPoints.Text = $"Points: Center ({centerPoint.Value.X}, {centerPoint.Value.Y})";
+            }
+            else if (circumferencePoint == null)
+            {
+                circumferencePoint = e.Location;
+                float radius = CalculateDistance((Point)centerPoint, (Point)circumferencePoint);
+                float area = (float)Math.PI * radius * radius;
+                lblPoints.Text += $", Circumference ({circumferencePoint.Value.X}, {circumferencePoint.Value.Y})";
+                lblRadius.Text = $"Radius: {radius:F2}";
+                lblArea.Text = $"Area: {area:F2}";
+                this.Invalidate();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            centerPoint = null;
+            circumferencePoint = null;
+            lblPoints.Text = "Points:";
+            lblRadius.Text = "Radius:";
+            lblArea.Text = "Area:";
+            this.Invalidate(); 
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (centerPoint != null && circumferencePoint != null)
+            {
+                float radius = CalculateDistance((Point)centerPoint, (Point)circumferencePoint);
+                e.Graphics.DrawEllipse(Pens.Black, centerPoint.Value.X - radius, centerPoint.Value.Y - radius, 2 * radius, 2 * radius);
+            }
+        }
+
+        private float CalculateDistance(Point p1, Point p2)
+        {
+            return (float)Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+        }
+    }
+}
